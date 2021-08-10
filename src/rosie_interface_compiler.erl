036ba9b214ab46d47a4a28f52b77ff5120775315
Luca Succi <luca.succi@studenti.unimi.it>
2021-08-10 21:42:26 +0200

    mmmm

--- src/rosie_interface_compiler.erl
+++ src/rosie_interface_compiler.erl
@@ -5,5 +5,5 @@
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
     {ok, State1} = compile_provider:init(State),
-    {ok, State2} = compile_provider:init(State1),
+    {ok, State2} = clean_provider:init(State1),
     {ok, State2}.
